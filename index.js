df8891cca6226861a321d9802ce6cbb72c86ac3f
flipott <ottphillipa@gmail.com>
2022-12-12 15:31:26 -0500

    Refactor site to use Express

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+// Express method
 
-http.createServer((req, res) => {
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
 
-    const file = url.parse(req.url, true);
-    let filename = "." + file.pathname + ".html";
-    if (filename === './.html') {filename = './index.html'}
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-    fs.readFile(filename, (err, data) => {
-        if (err) {
-            fs.readFile(__dirname + "/404.html", (err, data) => {
-                res.writeHead(404, {'Content-Type': 'text/html'})
-                res.write(data);
-                res.end()
-            })
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(data);
-            res.end();
-        }
-    })
-}).listen(8080);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'));
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '/404.html'));
+});
+
+app.listen(port, () => {console.log(`Server is running on localhost port ${port}.`)});
+
+// Plain Node method
+
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url');
+
+// http.createServer((req, res) => {
+
+//     const file = url.parse(req.url, true);
+//     let filename = "." + file.pathname + ".html";
+//     if (filename === './.html') {filename = './index.html'}
+
+//     fs.readFile(filename, (err, data) => {
+//         if (err) {
+//             fs.readFile(__dirname + "/404.html", (err, data) => {
+//                 res.writeHead(404, {'Content-Type': 'text/html'})
+//                 res.write(data);
+//                 res.end()
+//             })
+//         } else {
+//             res.writeHead(200, {'Content-Type': 'text/html'})
+//             res.end(data);
+//             res.end();
+//         }
+//     })
+// }).listen(8080);
